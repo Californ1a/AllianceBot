276603bcc2f47cedc777b898f4e57575a9580292
Californ1a <rk_california@yahoo.com>
2016-12-21 18:59:55 -0500

    add console log for presence update

--- bot.js
+++ bot.js
@@ -286,9 +286,13 @@ bot.on("presenceUpdate", (oldMember, newMember) => {
 			}
 
 			if (newMember.user.presence.game && newMember.user.presence.game.name === "Distance") {
-				newMember.addRole(playRole).catch(console.error);
+				newMember.addRole(playRole).then(
+					console.log(colors.white.dim(`${cl.getDisplayName(newMember)} has been added to the ${playRole.name} role.`));
+				).catch(console.error);
 			} else if ((!newMember.user.presence.game || !newMember.user.presence.game === "Distance") && newMember.roles.has(playRole.id)) {
-				newMember.removeRole(playRole).catch(console.error);
+				newMember.removeRole(playRole).then(
+					console.log(colors.white.dim(`${cl.getDisplayName(newMember)} has been removed from the ${playRole.name} role.`));
+				).catch(console.error);
 			}
 		}
 	}
