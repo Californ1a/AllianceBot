6f87a531b84c727b307338380d23434ab8eb7357
Californ1a <rk_california@yahoo.com>
2016-12-25 07:37:03 -0500

    attempt fix users keep playing distance role

--- bot.js
+++ bot.js
@@ -288,7 +288,9 @@ bot.on("presenceUpdate", (oldMember, newMember) => {
 
 			if (newMember.user.presence.game && newMember.user.presence.game.name === "Distance") {
 				newMember.addRole(playRole).then(console.log(colors.white.dim("* " + memberName + " added to " + playRole.name + " role on " + guild.name + " server."))).catch(console.error);
-			} else if ((!newMember.user.presence.game || !newMember.user.presence.game === "Distance") && newMember.roles.has(playRole.id)) {
+			} else if (!newMember.user.presence.game && newMember.roles.has(playRole.id)) {
+				newMember.removeRole(playRole).then(console.log(colors.white.dim("* " + memberName + " removed from " + playRole.name + " role on " + guild.name + " server."))).catch(console.error);
+			} else if ((newMember.user.presence.game && newMember.user.presence.game !== "Distance") && newMember.roles.has(playRole.id)) {
 				newMember.removeRole(playRole).then(console.log(colors.white.dim("* " + memberName + " removed from " + playRole.name + " role on " + guild.name + " server."))).catch(console.error);
 			}
 		}
