ae4baaf58bb67cbb8291f724edba29677e951328
Californ1a <rk_california@yahoo.com>
2017-02-24 13:45:54 -0500

    Add examples for gitignore'd files

---
+++ logins/sqlconfig_example.js
@@ -0,0 +1,7 @@
+module.exports = {
+	host: "localhost",
+	user: "USERNAME",
+	password: "PASSWORD",
+	database: "DATABASENAME",
+	charset: "utf8mb4"
+};
---
+++ logins/twitconfig_example.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key: "CONSUMERKEY",
+	consumer_secret: "CONSUMERSECRET",
+	access_token: "ACCESSTOKEN",
+	access_token_secret: "ACCESSTOKENSECRET"
+};
