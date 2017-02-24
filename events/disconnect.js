a1e2e52c43a54d7999500fd3ee5098f6c5cb2dc6
Californ1a <rk_california@yahoo.com>
2017-02-24 13:43:30 -0500

    Process exit on disconnect, for pm2 to restart

--- events/disconnect.js
+++ events/disconnect.js
@@ -2,4 +2,5 @@ const colors = require("colors");
 module.exports = (bot, closeEvent) => {
 	console.log(colors.red(`Bot disconnected from server with status code ${closeEvent.code}.`));
 	console.log(`Reason: ${closeEvent.reason}`);
+	process.exit();
 };
