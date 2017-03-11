4b3ed641c00cbfc0019936089af4cb96ad1e26a9
Californ1a <rk_california@yahoo.com>
2017-03-11 02:07:21 -0500

    missing =

--- events/ready.js
+++ events/ready.js
@@ -25,7 +25,7 @@ var randomNickChange = function(bot) {
 };
 
 module.exports = bot => {
-	console.log(colors.red(`Bot online and ready on ${bot.guilds.size} ${(bot.guilds.size > 2) ? "servers" : "server"}.`));
+	console.log(colors.red(`Bot online and ready on ${bot.guilds.size} ${(bot.guilds.size >= 2) ? "servers" : "server"}.`));
 	bot.user.setStatus("online").catch(e => console.error(e.stack));
 	bot.user.setGame(`Distance | ${pre}help`).catch(e => console.error(e.stack));
 	randomNickChange(bot);
