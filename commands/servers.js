47bf14b8f365f5b53750a795b5c2bb95b6e5e305
Californ1a <rk_california@yahoo.com>
2018-04-19 20:26:28 -0400

    change message on 1 server with open slots

--- commands/servers.js
+++ commands/servers.js
@@ -24,7 +24,7 @@ exports.run = (bot, msg) => {
 		const pubServs = servs.filter(s => !s.passwordProtected);
 		const openPubs = pubServs.filter(s => s.connectedPlayers < s.playerLimit);
 		const totalSlots = openPubs.reduce((acc, obj) => acc + (obj.playerLimit - obj.connectedPlayers), 0);
-		send(msg.channel, `There ${(pubServs.length === 1) ? "is 1 public server" : `are ${pubServs.length} public servers`}${(pubServs.length > 0 && openPubs.length > 0) ? `, ${openPubs.length} of which account for a combined ${totalSlots} open slots.` : (pubServs.length > 0) ? " with no open slots.":"."}`);
+		send(msg.channel, `There ${(pubServs.length === 1) ? "is 1 public server" : `are ${pubServs.length} public servers`}${(pubServs.length > 0 && openPubs.length > 0) ? (openPubs.length < 2) ? ` with ${totalSlots} open slots.` : `, ${openPubs.length} of which account for a combined ${totalSlots} open slots.` : (pubServs.length > 0) ? " with no open slots.":"."}`);
 	}).catch(() => send(msg.channel, "Failed to obtain server list."));
 };
 
