0ad22d4b50a5d6febe27d6f0af44037cd0902866
Californ1a <rk_california@yahoo.com>
2018-08-05 15:18:29 -0400

    fix checking for timeout role on newly-joined server members

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,13 +1,15 @@
 const canAssignRole = require("../util/canAssignRole.js");
 
 module.exports = (bot, member) => {
+	console.log(`${member.displayName} joined ${member.guild.name} server.`);
 	const conf = bot.servConf.get(member.guild.id);
 	const botMember = member.guild.members.get(bot.user.id);
 	const toRole = (conf.timeoutrole) ? member.guild.roles.find("name", conf.timeoutrole) : member.guild.roles.find("name", "Timeout");
 	if (!toRole || !canAssignRole(botMember, member)) {
 		return;
 	}
-	if (bot.timer.get(member.id) && !member.roles.find("name", toRole)) {
+	if (bot.timer.get(member.id) && !member.roles.get(toRole.id)) {
 		member.addRole(toRole);
+		console.log(`${member.displayName} re-added to timeout in ${member.guild.name} server.`);
 	}
 };
