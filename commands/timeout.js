8b19e0526e6cae4107dd9d575fbfea22ea55fe06
Californ1a <rk_california@yahoo.com>
2018-08-05 14:50:11 -0400

    fix manually removing timeout

--- commands/timeout.js
+++ commands/timeout.js
@@ -18,7 +18,7 @@ exports.run = (bot, msg, args) => {
 		return send(msg.channel, `The Timeout role could not be found. Make sure a Timeout role is set in your server config: \`${pre}config --to <role name here>\``);
 	}
 	if (msgMember.highestRole.hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) {
-		if (!(args.length >= 2) || !msg.mentions.users.first()) {
+		if (!msg.mentions.users.first()) {
 			return send(msg.channel, `Incorrect syntax. Use \`${prefix}help timeout\` for syntax.`);
 		}
 		const mentionedMember = msg.guild.members.get(msg.mentions.users.first().id);
@@ -32,6 +32,9 @@ exports.run = (bot, msg, args) => {
 			} else {
 				return send(msg.channel, `${mentionedMember.displayName} does not have the Timeout role.`);
 			}
+		}
+		if (!(args.length >= 2)) {
+			return send(msg.channel, `Incorrect syntax. Use \`${prefix}help timeout\` for syntax.`);
 		}
 		if (!canUserAndBotAssign(msgMember, mentionedMember, botMember)) {
 			return send(msg.channel, "Either the bot or you do not have permission to perform this action.");
