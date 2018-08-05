0bde2b5da27642e424c59c7b50ac220f10820342
Californ1a <rk_california@yahoo.com>
2018-08-05 00:44:32 -0400

    fix undefined member

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,6 +1,9 @@
 const cl = require("../util/chatinfo.js");
 
 module.exports = (bot, oldMember, newMember) => {
+	if (!oldMember || !newMember || !oldMember.displayName || !newMember.displayName) {
+		return;
+	}
 	if (oldMember.displayName !== newMember.displayName && !newMember.user.bot) {
 		cl.writeLineToAllLogs(bot, newMember.guild, `${oldMember.displayName} is now known as ${newMember.displayName}`);
 	}
