98ac07211bb2d860fee5433596ef0a74b971c484
Californ1a <rk_california@yahoo.com>
2018-09-24 09:17:08 -0400

    remove highestrole has permission, check member perm instead

--- commands/timeout.js
+++ commands/timeout.js
@@ -8,59 +8,59 @@ const canUserAndBotAssign = require("../util/canAssignRole.js");
 require("../util/Array.prototype.rejoin.js");
 
 exports.run = (bot, msg, args) => {
-	const pre = bot.servConf.get(msg.guild.id).prefix;
+	const conf = bot.servConf.get(msg.guild.id);
+	const pre = conf.prefix;
 	const msgMember = msg.member;
 	const botMember = msg.guild.members.get(bot.user.id);
-	const conf = bot.servConf.get(msg.guild.id);
+	if (!msgMember.hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) {
+		return;
+	}
 	const toRole = (conf.timeoutrole) ? msg.guild.roles.find(val => val.name === conf.timeoutrole) : msg.guild.roles.find(val => val.name === "Timeout");
-	const prefix = conf.prefix;
 	if (!toRole) {
 		return send(msg.channel, `The Timeout role could not be found. Make sure a Timeout role is set in your server config: \`${pre}config --to <role name here>\``);
 	}
-	if (msgMember.highestRole.hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) {
-		if (!msg.mentions.users.first()) {
-			return send(msg.channel, `Incorrect syntax. Use \`${prefix}help timeout\` for syntax.`);
-		}
-		const mentionedMember = msg.guild.members.get(msg.mentions.users.first().id);
-		if (msg.mentions.users.first() && args.length === 1) {
-			if (bot.timer.get(mentionedMember.id)) {
-				clearTimeout(bot.timer.get(mentionedMember.id));
-			}
-			if (mentionedMember.roles.get(toRole.id)) {
-				manageTimeout(mentionedMember, bot, toRole, msg.guild.id);
-				return send(msg.channel, `${mentionedMember.displayName} was manually removed from timeout.`);
-			} else {
-				return send(msg.channel, `${mentionedMember.displayName} does not have the Timeout role.`);
-			}
-		}
-		if (!(args.length >= 2)) {
-			return send(msg.channel, `Incorrect syntax. Use \`${prefix}help timeout\` for syntax.`);
-		}
-		if (!canUserAndBotAssign(msgMember, mentionedMember, botMember)) {
-			return send(msg.channel, "Either the bot or you do not have permission to perform this action.");
-		}
-		const duration = args.rejoin(" ", 1);
-		const durationMS = parse(duration);
-		if (durationMS < 0) {
-			return send(msg.channel, "The duration must be positive.");
-		}
-		const d = new Duration(`${durationMS}ms`);
-		const now = Date.now();
-		const later = new Date(now + d);
-		const info = {
-			memberid: mentionedMember.id,
-			"server_id": msg.guild.id,
-			enddate: later
-		};
-		connection.insert("timeout", info).then(() => {
-			mentionedMember.addRole(toRole);
-			send(msg.channel, `${mentionedMember.displayName} has been put in timeout for ${d.toString()}.`);
-			const toTimer = setTimeout(() => {
-				manageTimeout(mentionedMember, bot, toRole, msg.guild.id);
-			}, d);
-			bot.timer.set(mentionedMember.id, toTimer);
-		});
+	if (!msg.mentions.users.first()) {
+		return send(msg.channel, `Incorrect syntax. Use \`${pre}help timeout\` for syntax.`);
 	}
+	const mentionedMember = msg.guild.members.get(msg.mentions.users.first().id);
+	if (msg.mentions.users.first() && args.length === 1) {
+		if (bot.timer.get(mentionedMember.id)) {
+			clearTimeout(bot.timer.get(mentionedMember.id));
+		}
+		if (mentionedMember.roles.get(toRole.id)) {
+			manageTimeout(mentionedMember, bot, toRole, msg.guild.id);
+			return send(msg.channel, `${mentionedMember.displayName} was manually removed from timeout.`);
+		} else {
+			return send(msg.channel, `${mentionedMember.displayName} does not have the Timeout role.`);
+		}
+	}
+	if (!(args.length >= 2)) {
+		return send(msg.channel, `Incorrect syntax. Use \`${pre}help timeout\` for syntax.`);
+	}
+	if (!canUserAndBotAssign(msgMember, mentionedMember, botMember)) {
+		return send(msg.channel, "Either the bot or you do not have permission to perform this action.");
+	}
+	const duration = args.rejoin(" ", 1);
+	const durationMS = parse(duration);
+	if (durationMS < 0) {
+		return send(msg.channel, "The duration must be positive.");
+	}
+	const d = new Duration(`${durationMS}ms`);
+	const now = Date.now();
+	const later = new Date(now + d);
+	const info = {
+		memberid: mentionedMember.id,
+		"server_id": msg.guild.id,
+		enddate: later
+	};
+	connection.insert("timeout", info).then(() => {
+		mentionedMember.addRole(toRole);
+		send(msg.channel, `${mentionedMember.displayName} has been put in timeout for ${d.toString()}.`);
+		const toTimer = setTimeout(() => {
+			manageTimeout(mentionedMember, bot, toRole, msg.guild.id);
+		}, d);
+		bot.timer.set(mentionedMember.id, toTimer);
+	});
 };
 
 exports.conf = {
