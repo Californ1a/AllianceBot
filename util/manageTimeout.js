a16ede3208671feb65f80e1d1ffe58b01f75ac3e
Californ1a <rk_california@yahoo.com>
2018-10-26 18:08:45 -0400

    fix bootloop

--- util/manageTimeout.js
+++ util/manageTimeout.js
@@ -3,11 +3,13 @@ const connection = require("./connection.js");
 
 function manageTimeout(mentionedMember, bot, toRole, guildid) {
 	const guild = bot.guilds.get(guildid);
-	if (mentionedMember && guild.members.get(mentionedMember.id)) {
-		mentionedMember.removeRole(toRole).catch(e => console.error(e.stack));
-		console.log(colors.red(`${mentionedMember.displayName} removed from timeout.`));
-	} else if (mentionedMember) {
-		console.log(colors.red(`When attempting to remove Timeout role, ${mentionedMember.displayName} could not be found.`));
+	if (mentionedMember) {
+		if (guild.members.get(mentionedMember.id)) {
+			mentionedMember.removeRole(toRole).catch(e => console.error(e.stack));
+			console.log(colors.red(`${mentionedMember.displayName} removed from timeout.`));
+		} else {
+			console.log(colors.red(`When attempting to remove Timeout role, ${mentionedMember.displayName} could not be found.`));
+		}
 	} else {
 		console.log(colors.red("When attempting to remove Timeout role, the member could not be found on the server anymore."));
 	}
